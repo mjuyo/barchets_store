41ea0fd523b78ca8b5ee79b6df65873d3d02ae5e
mjuyo <150392024+mjuyo@users.noreply.github.com>
2024-07-11 16:26:41 -0500

    Fixed bug with Categories in ActiveAdmin, issue with the Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
-    before_action :set_categories
+    before_action :set_categories, unless: :admin_controller?
 
     private
 
     def set_categories
         @categories = Category.all
+    end
+
+    def admin_controller?
+        self.class < ActiveAdmin::BaseController
     end
 end
